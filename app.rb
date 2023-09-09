5b7c35f6600dd4fcdcbaa0c7839b8b29c51b0b88
Dmitrii Sumenko <64180264+flstudio4@users.noreply.github.com>
2023-09-09 19:08:20 +0000

    Final changes

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require "sinatra"
 require "sinatra/reloader"
+require "net/http"
+require "json"
 
 get("/") do
   erb(:main)
+end
+
+post("/generate") do
+  url = URI("https://api.thecatapi.com/v1/images/search")
+  raw_data = Net::HTTP.get(url)
+  parsed_data = JSON.parse(raw_data)
+  @pic_url = parsed_data[0].fetch("url").to_s
+  erb(:generate)
 end
